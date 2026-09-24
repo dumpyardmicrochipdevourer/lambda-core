ef60ee2550fcfa7a3507f08ca2ab8a07dbc3f88b
anton <russiansoveet@gmail.com>
2026-09-24 10:03:18 +0300

    [init] initialize project

---
+++ src/main/java/com/microchip/lambda_core/LambdaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.microchip.lambda_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaCoreApplication.class, args);
+	}
+
+}
